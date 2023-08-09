03f32871c01f8d952a533b78b5a5c19d5bd01b3e
rabhisheksps <abhishek_rastogi@softprodigy.com>
2023-08-09 15:40:37 +0530

    added routes and buttons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -34,12 +34,9 @@ export default function Footer(props) {
           <div className="row mt-3">
             <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
               <h6 className="text-uppercase fw-bold mb-4">
-                <i className="fa fa-gem me-3 text-secondary"></i>Company name
+                <i className="fa fa-gem me-3 text-secondary"></i>Text-Utils
               </h6>
-              <p>
-                Here you can use rows and columns to organize your footer content. Lorem ipsum
-                dolor sit amet, consectetur adipisicing elit.
-              </p>
+              <p>TextUtils is an open-source text utility app which offer various day-to-day use features such as word & character counting, covert text to UpperCase and much more.</p>
             </div>
             <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
               <h6 className="text-uppercase fw-bold mb-4">
@@ -53,9 +50,6 @@ export default function Footer(props) {
               </p>
               <p>
                 <a href="#!" className="text-reset">Vue</a>
-              </p>
-              <p>
-                <a href="#!" className="text-reset">Laravel</a>
               </p>
             </div>
             <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
@@ -71,9 +65,6 @@ export default function Footer(props) {
               <p>
                 <a href="#!" className="text-reset">Orders</a>
               </p>
-              <p>
-                <a href="#!" className="text-reset">Help</a>
-              </p>
             </div>
             <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
               <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
@@ -83,14 +74,12 @@ export default function Footer(props) {
                 contact@textutils.com
               </p>
               <p><i className="fa fa-phone me-3 text-secondary"></i> + 01 234 567 88</p>
-              <p><i className="fa fa-print me-3 text-secondary"></i> + 01 234 567 89</p>
             </div>
           </div>
         </div>
       </section>
-      <div className="text-center p-4" style={{backgroundColor: 'rgba(0, 0, 0, 0.025)'}}>
-        © 2023 Copyright:
-        <a className="text-reset fw-bold" href="#!"><p style={{color: props.mode==='dark'?'white':'black'}}>TextUtils.com</p></a>
+      <div className="text-center p-2" style={{backgroundColor: 'rgba(0, 0, 0, 0.025)'}}>
+        <p style={{color: props.mode==='dark'?'white':'black'}}>© 2023 Copyright: TextUtils.com</p>
       </div>
     </footer>
     </div>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -10,30 +10,44 @@ export default function TextForm(props) {
     // console.log("Uppercase button is clicked");
     let newText = text.toUpperCase();
     setText(newText);
+    props.showAlert("Your text has been converted to upper case.", "success")
   }
   const handleDownClick = () => {
     // console.log("Uppercase button is clicked");
     let newText = text.toLowerCase();
     setText(newText);
+    props.showAlert("Your text has been converted to lower case.", "success")
   }
   const handleClear = () => {
     let newText = ('');
-    setText(newText)
+    setText(newText);
+    props.showAlert("Your text has been cleared.", "success")
   }
   const speak = () => {
     let msg = new SpeechSynthesisUtterance();
-    msg.text = text;
     window.speechSynthesis.speak(msg);
   }
   const handleCopy = () => {
     var text = document.getElementById("myBox");
     text.select();
-    text.setSelectionRange(0, 9999);
     navigator.clipboard.writeText(text.value)
+    props.showAlert("Your text has been copied to clipboard.", "success")
   }
   const handleExtraSpaces = () => {
     let newText = text.split(/[  ]+/);
-    setText(newText.join(" "))
+    setText(newText.join(" "));
+    props.showAlert("All the extra spaces in the text have been removed.", "success")
+  }
+  const capitalizeFirstLetter = (word) => {
+    return word?.charAt(0).toUpperCase() + word?.slice(1).toLowerCase();
+  }
+
+  const handleCapitalize = () => {
+    let newText = text?.split(' ');
+    const capitalized_words = newText?.map(word => capitalizeFirstLetter(word));
+    const finalOutput = capitalized_words.join(' ')    
+    setText(finalOutput)
+    props.showAlert("First alphabet of all letter are now uppercase.", "success")
   }
   return (
     <>
@@ -48,13 +62,14 @@ export default function TextForm(props) {
         <button className="btn btn-primary mx-2" onClick={speak}>Speak</button>
         <button className='btn btn-primary' onClick={handleCopy}>Copy Text</button>
         <button className='btn btn-primary mx-2' onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+        <button className='btn btn-primary' onClick={handleCapitalize}>Capitalize First Alphabet</button>
       </div>
       <div className="container my-3" style={{color: props.mode==='dark'?'white':'black'}}>
         <h2>Your text summary:</h2>
-        <p><strong>{text.split(" ").length}</strong> words, <strong>{text.length}</strong> characters.</p>
-        <p><strong>{(0.08 * text.split(" ").length).toFixed(3)}</strong> minutes read.</p>
+        <p><strong>{text?.split(" ")?.length}</strong> words, <strong>{text?.length}</strong> characters.</p>
+        <p><strong>{(0.08 * text?.split(" ").length)?.toFixed(3)}</strong> minutes read.</p>
         <h3><strong>Preview</strong>:</h3>
-        <p style={{width: '80%'}}>{text.length>0?text.slice(0, 2500):"Enter your text to preview here"}</p>
+        <p style={{width: '80%'}}>{text?.length>0?text.slice(0, 2500):"Enter your text to preview here"}</p>
       </div>
     </>
   )
